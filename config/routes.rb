630fc7dcacecf5936335ae53fdcd04094ed1f335
motokitakashima <pageboy.jr@icloud.com>
2021-05-09 11:31:17 +0900

    ゲストユーザーが削除されない処理を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  devise_for :users
   root "homes#index"
   resources :posts
+  devise_for :users, controllers: {
+    registrations: "users/registrations"
+  }
   devise_scope :user do
     post "users/guest_sign_in", to: "users/sessions#guest_sign_in"
   end
