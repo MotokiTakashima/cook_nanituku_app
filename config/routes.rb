5873cc554f82aee3f7e2aeed05a07c00fdf5a3ee
motokitakashima <pageboy.jr@icloud.com>
2021-05-09 10:57:12 +0900

    routes.rb にguest_sign_in を追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root "homes#index"
   resources :posts
+  devise_scope :user do
+    post "user/guest_sign_in", to: "users/session#guest_sign_in"
+  end
 end
